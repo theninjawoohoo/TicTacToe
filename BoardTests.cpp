3cc6d3aae2074281d3da768da2bb57c01f2599fa
isose <theisose@gmail.com>
2018-02-16 15:41:23 -0800

    Added tests for Board::placeMove

--- BoardTests.cpp
+++ BoardTests.cpp
@@ -1,6 +1,26 @@
 #include "catch.hpp"
 #include "Board.h"
 
-TEST_CASE("Example Test") {
-	REQUIRE(6 == 2*3);
+TEST_CASE("placeMove") {
+	Board b;
+
+	SECTION("Out of bounds: x < 0") {
+		REQUIRE(b.placeMove(-1, 0, 'X') == false);
+	}
+
+	SECTION("Out of bounds: x > BOARD_SIZE") {
+		REQUIRE(b.placeMove(3, 0, 'X') == false);
+	}
+
+	SECTION("Out of bounds: y < 0") {
+		REQUIRE(b.placeMove(0, -1, 'X') == false);
+	}
+
+	SECTION("Out of bounds: y > BOARD_SIZE") {
+		REQUIRE(b.placeMove(0, 3, 'X') == false);
+	}
+
+	SECTION("In bounds") {
+		REQUIRE(b.placeMove(0, 0, 'X') == true);
+	}
 }
