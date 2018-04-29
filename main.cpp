d5252277e88b4997f81545c85f86c07da9784d89
isose <theisose@gmail.com>
2018-04-29 01:59:16 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,8 @@
 using namespace std;
 
 int main() {
-	Board board;
-	board.placeMove(0, 0, 'X');
-	board.placeMove(1, 0, 'X');
-	board.placeMove(2, 0, 'O');
-	board.placeMove(0, 1, 'O');
-	board.placeMove(1, 1, 'O');
-	board.placeMove(2, 1, 'X');
-	board.placeMove(0, 2, 'X');
-	board.placeMove(1, 2, 'X');
-	board.placeMove(2, 2, 'O');
-
-	board.printBoard();
-
-
-	cout << "Winner? " << board.checkForWinner() << endl;
+	GameLoop game;
+	game.run();
 
 	return 0;
 }
